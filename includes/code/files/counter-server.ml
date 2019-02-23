2a52465b91be0c98b9fa771af16980e042a82527
Daniil Baturin <daniil@baturin.org>
2019-02-23 09:24:11 +0700

    Updated the Lwt counter server to build with Lwt 4.1.0 and the Logs library instead of formerly built-in Lwt_log.

--- includes/code/files/counter-server.ml
+++ includes/code/files/counter-server.ml
@@ -2,7 +2,7 @@
 
     Clients can increment a shared counter or read its current value.
 
-    Build with: ocamlfind ocamlopt -package lwt,lwt.unix -linkpkg -o counter-server ./counter-server.ml
+    Build with: ocamlfind ocamlopt -package lwt,lwt.unix,logs,logs.lwt -linkpkg -o counter-server ./counter-server.ml
  *)
 
 open Lwt
@@ -14,7 +14,6 @@ let listen_address = Unix.inet_addr_loopback
 let port = 9000
 let backlog = 10
 
-let () = Lwt_log.add_rule "*" Lwt_log.Info
 
 let handle_message msg =
     match msg with
@@ -29,14 +28,14 @@ let rec handle_connection ic oc () =
         | Some msg -> 
             let reply = handle_message msg in
             Lwt_io.write_line oc reply >>= handle_connection ic oc
-        | None -> Lwt_log.info "Connection closed" >>= return)
+        | None -> Logs_lwt.info (fun m -> m "Connection closed") >>= return)
 
 let accept_connection conn =
     let fd, _ = conn in
     let ic = Lwt_io.of_fd Lwt_io.Input fd in
     let oc = Lwt_io.of_fd Lwt_io.Output fd in
-    Lwt.on_failure (handle_connection ic oc ()) (fun e -> Lwt_log.ign_error (Printexc.to_string e));
-    Lwt_log.info "New connection" >>= return
+    Lwt.on_failure (handle_connection ic oc ()) (fun e -> Logs.err (fun m -> m "%s" (Printexc.to_string e) ));
+    Logs_lwt.info (fun m -> m "New connection") >>= return
  
 let create_socket () =
     let open Lwt_unix in
@@ -51,7 +50,8 @@ let create_server sock =
     in serve
 
 let () =
+    let () = Logs.set_reporter (Logs.format_reporter ()) in
+    let () = Logs.set_level (Some Logs.Info) in
     let sock = create_socket () in
     let serve = create_server sock in
     Lwt_main.run @@ serve ()
-
