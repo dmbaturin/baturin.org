670490cdc568bdee3056b6dc4a66643c02cd39c4
Daniil Baturin <daniil@baturin.org>
2021-08-15 00:03:56 +0700

    Improvements to the music page generator plugin.

--- plugins/music-page-generator.lua
+++ plugins/music-page-generator.lua
@@ -34,13 +34,12 @@ function generate_piece(entry)
     local file = entry["file"]
 
     -- Insert the header
-    HTML.append_child(div, HTML.parse(format([[<h3 id="%s" class="composition-title">%s</h3>]],
-      String.slugify_ascii(entry["title"]), entry["title"])))
-    HTML.set_attribute(div, "id", "_" .. String.slugify_ascii(entry["title"]))
+    HTML.append_child(div, HTML.parse(format([[<h3 id="%s" class="composition-title"><a class="here" href="#%s">→</a> %s</h3>]],
+      entry["file"], entry["file"], entry["title"])))
+    HTML.set_attribute(div, "id", "_" .. String.slugify_ascii(entry["file"]))
 
     -- Insert the playable audio
     sound_file = add_extension(file, sound_extension)
-    Log.debug(Sys.join_path(dir, sound_file))
     if Sys.file_exists(Sys.join_path(dir, sound_file)) then
       audio_elem = HTML.parse(format([[<audio src="%s" controls=""></audio>]], Sys.join_path(url_path, sound_file)))
       HTML.append_child(div, audio_elem)
@@ -49,11 +48,11 @@ function generate_piece(entry)
     HTML.append_child(div, HTML.parse(entry["description"]))
 
     -- Insert date
-    HTML.append_child(div, HTML.parse(format([[<p>Date: %s.</p>]], entry["date"])))
+    HTML.append_child(div, HTML.parse(format([[<p><strong>Date</strong>: %s.</p>]], entry["date"])))
 
     -- Insert download links
     links_p = HTML.create_element("p")
-    HTML.append_child(links_p, HTML.create_text("Download: "))
+    HTML.append_child(links_p, HTML.parse("<strong>Download:</strong> "))
     local k = 1
     local n = 1
     local links = {}
@@ -61,18 +60,13 @@ function generate_piece(entry)
       local ext = score_extensions[k]
       Log.debug(ext)
       if Sys.file_exists(Sys.join_path(dir, add_extension(file, ext))) then
-        Log.debug("making link")
         link = format([[<a href="%s">%s</a>]], Sys.join_path(url_path, add_extension(file, ext)), mapping[ext])
-        Log.debug("inserring link")
         links[n] = link
         n = n + 1
       end
       k = k + 1
     end
-    Log.debug(JSON.pretty_print(links))
     links_src = String.join(", ", links)
-    Log.debug("aboiut to parse")
-    Log.debug(links_src)
     HTML.append_child(links_p, HTML.parse(links_src))
     HTML.append_child(div, links_p)
   else
@@ -82,20 +76,7 @@ function generate_piece(entry)
   HTML.append_child(container, div)
 end
 
-Log.debug("reading data")
 data = TOML.from_string(Sys.read_file(config["data_file"]))
 data = data["compositions"]
---Log.debug(JSON.pretty_print(data))
 
-Log.debug("generating pieces")
-
--- Table.iter doesn't make any guarantees about the order,
--- so we can't use it and have to enforce original ordering
--- with hand-cranked iteration.
-n = 1
-while data[n] do
-  generate_piece(data[n])
-  n = n + 1
-end
-
---Plugin.fail("expected")
+Table.iter_values_ordered(generate_piece, data)
